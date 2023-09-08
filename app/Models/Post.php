3e98842a2c59dc9f53aa6c9aa59462759efc8890
deniskhen <denis.khen79@mail.ru>
2023-09-08 20:21:37 +0300

    связь м-к-м категорий с постами

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 //PHP docs
@@ -29,5 +30,10 @@ class Post extends Model
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class, foreignKey: 'post_id', localKey: 'id');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
     }
 }
